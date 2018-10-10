d1dae0b0307b32618c22b79ac59f0919dd8b02b6
José Ballester <jose@ballester.me>
2018-10-10 14:43:07 -0400

    Single Python script for long tests

---
+++ src/example-block.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import os, sys
+import numpy as np
+import rospy
+from sensor_msgs.msg import JointState
+from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Header
+
+# All sizes in mm!
+hz = 250.0 # hz
+config = [
+    [0.0, 5.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [-30.0, 5.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [5.0, 10.0],
+    [0.0, 3.0],
+    [-20.0, 5.0]
+]
+
+if __name__ == '__main__':
+    rospy.init_node('example-block', anonymous=True)
+    command_pose_pub = rospy.Publisher('/command_pose', PoseStamped, queue_size = 100, latch=True)
+
+    rospy.set_param('egm_mode', 'velocity')
+
+    rate = rospy.Rate(hz)
+    start_time = rospy.Time.now().to_sec()
+    i = 0
+
+    while (not rospy.is_shutdown()) and i < len(config):
+        pose = PoseStamped()
+        pose.header = Header()
+        pose.header.stamp = rospy.Time.now()
+        pose.header.frame_id = "map"
+        # Position in mm or velocity in mm/s
+        pose.pose.position.x = config[i][0]
+        pose.pose.position.y = 0.0
+        pose.pose.position.z = 0.0
+        # Orientation or angular velocity in xyzw
+        pose.pose.orientation.x = 0.0
+        pose.pose.orientation.y = 0.0
+        pose.pose.orientation.z = 0.0
+        pose.pose.orientation.w = 0.0
+        command_pose_pub.publish(pose)
+
+        if rospy.Time.now().to_sec()-start_time < config[i][1]:
+            i += 1
+
+        rate.sleep()
---
+++ test/stats_bag.py
@@ -0,0 +1,56 @@
+#!/bin/python
+import rosbag
+import rospy
+import numpy as np
+import matplotlib.pyplot as plt
+
+bag = rosbag.Bag('2018-10-10-11-20-33.bag')
+speed = 5.0
+pre_time = 0.5
+time = 10.5
+
+start_times = []
+sent_poses_times = []
+sent_poses = []
+prev_seq = 1e9
+
+for topic, msg, t in bag.read_messages(topics='/command_pose'):
+	if prev_seq > msg.header.seq and msg.pose.position.x == speed:
+		start_times.append(t)
+	prev_seq = msg.header.seq
+
+for start_time in start_times:
+	t0 = []
+	x0 = []
+	t1 = []
+	x1 = []
+	t2 = []
+	x2 = []
+	xref = 0.0
+
+	for topic, msg, t in bag.read_messages(topics='/command_pose', start_time=start_time-rospy.rostime.Duration.from_sec(pre_time), end_time=start_time+rospy.rostime.Duration.from_sec(time)):
+		t0.append(t.to_sec()-start_time.to_sec())
+		x0.append(msg.pose.position.x)
+
+	for topic, msg, t in bag.read_messages(topics='/sent_pose', start_time=start_time-rospy.rostime.Duration.from_sec(pre_time), end_time=start_time+rospy.rostime.Duration.from_sec(time)):
+		if xref == 0.0:
+			xref = msg.pose.position.x
+		t1.append(t.to_sec()-start_time.to_sec())
+		x1.append(msg.pose.position.x-xref)
+
+	for topic, msg, t in bag.read_messages(topics='/measured_pose', start_time=start_time-rospy.rostime.Duration.from_sec(pre_time), end_time=start_time+rospy.rostime.Duration.from_sec(time)):
+		t2.append(t.to_sec()-start_time.to_sec())
+		x2.append(msg.pose.position.x-xref)
+
+	print(t1[np.min(np.where(np.array(x1) > x1[0]))]-t0[0])
+	ax = plt.subplot(111)
+	ax.plot(t0, x0, label='Expected position (theoretical), '+str(speed)+' mm/s')
+	ax.plot(t1, x1, label='Position (sent), '+str(speed)+' mm/s')
+	ax.plot(t2, x2, label='Position (measured), '+str(speed)+' mm/s')
+	# ax.plot(t1, [t*speed for t in t1], '--', label='Constant speed at '+str(speed)+' mm/s')
+	plt.xlabel('Time (s)')
+	plt.ylabel('Traveled distance (mm)')
+	chartBox = ax.get_position()
+	ax.set_position([chartBox.x0, chartBox.y0, chartBox.width*0.6, chartBox.height])
+	ax.legend(loc='upper center', bbox_to_anchor=(1.45, 0.8), shadow=True, ncol=1)
+	plt.show()
